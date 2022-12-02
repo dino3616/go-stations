2be2eefd990fe981419e739f4fee634011de27eb
shio <85730998+dino3616@users.noreply.github.com>
2022-12-02 12:22:07 +0000

    feat: ✨ (service/todo) TODOService.DeleteTODOを作成

--- service/todo.go
+++ service/todo.go
@@ -3,6 +3,8 @@ package service
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/TechBowl-japan/go-stations/model"
 	"github.com/mattn/go-sqlite3"
@@ -140,7 +142,34 @@ func (s *TODOService) UpdateTODO(ctx context.Context, id int64, subject, descrip
 
 // DeleteTODO deletes TODOs on DB by ids.
 func (s *TODOService) DeleteTODO(ctx context.Context, ids []int64) error {
-	const deleteFmt = `DELETE FROM todos WHERE id IN (?%s)`
+	if len(ids) == 0 {
+		return nil
+	}
+
+	const deleteFmt = `DELETE FROM todos WHERE id IN (%s)`
+
+	args := make([]interface{}, 0, len(ids))
+	for _, id := range ids {
+		args = append(args, id)
+	}
+
+	res, err := s.db.ExecContext(ctx, fmt.Sprintf(deleteFmt, strings.TrimRight(strings.Repeat("?,", len(ids)), ",")), args...)
+	if err != nil {
+		return err
+	}
+
+	n, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if n == 0 {
+		return &model.ErrNotFound{
+			Sqlite3Error: sqlite3.Error{
+				Code:         sqlite3.ErrConstraint,
+				ExtendedCode: sqlite3.ErrConstraintUnique,
+			},
+		}
+	}
 
 	return nil
 }
