ed4c27f4820130c4d5bc8b9877a7a5181b7d5129
shio <85730998+dino3616@users.noreply.github.com>
2022-12-02 07:12:55 +0000

    feat: ✨ (model/error) Unwrapメソッドを追加

--- model/error.go
+++ model/error.go
@@ -1,7 +1,15 @@
 package model
 
-type ErrNotFound struct{}
+import "github.com/mattn/go-sqlite3"
+
+type ErrNotFound struct {
+	Sqlite3Error sqlite3.Error
+}
 
 func (e *ErrNotFound) Error() string {
-	return "not found"
+	return e.Sqlite3Error.Error()
+}
+
+func (e *ErrNotFound) Unwrap() error {
+	return e.Sqlite3Error
 }
